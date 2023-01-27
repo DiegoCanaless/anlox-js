5c1818d113d0bcc7e68bc99e77a71bc1c8f6d565
Diego Canales <diegocanales112@gmail.com>
2023-01-27 17:04:38 -0300

    Se rehizo el funcionamiento de las cards con feths

--- js/scriptProductos.js
+++ js/scriptProductos.js
@@ -1,110 +1,10 @@
-const stock = [
-    {
-        id: 1,
-        nombre: "Campera Quantum",
-        precio: 8000,
-        img: "../assets/foto-productos/quantum.jpg",
-        cantidad: 1
-    },
-    {
-        id: 2,
-        nombre: "Campera Ashley",
-        precio: 12000,
-        img: "../assets/foto-productos/ashley.jpg",
-        cantidad: 1
-    },
-    {
-        id: 3,
-        nombre: "Campera Rasmus",
-        precio: 8000,
-        img: "../assets/foto-productos/rasmus.jpg",
-        cantidad: 1
-    },
-    {
-        id: 4,
-        nombre: "Campera Max",
-        precio: 5000,
-        img: "../assets/foto-productos/max.jpg",
-        cantidad: 1
-    },
-    {
-        id: 5,
-        nombre: "Campera Garbi",
-        precio: 6000,
-        img: "../assets/foto-productos/garbi.jpg",
-        cantidad: 1
-    },
-    {
-        id: 6,
-        nombre: "Campera Erin",
-        precio: 8000,
-        img: "../assets/foto-productos/erin.jpg",
-        cantidad: 1
-    },
-    {
-        id: 7,
-        nombre: "Campera Ivor",
-        precio: 12000,
-        img: "../assets/foto-productos/ivor.jpg",
-        cantidad: 1
-    },
-    {
-        id: 8,
-        nombre: "Campera Vayne",
-        precio: 10000,
-        img: "../assets/foto-productos/vayne.jpg",
-        cantidad: 1
-    },
-    {
-        id: 9,
-        nombre: "Campera Moror",
-        precio: 14000,
-        img: "../assets/foto-productos/moror.jpg",
-        cantidad: 1
-    },
-    {
-        id: 10,
-        nombre: "Campera Crizzy",
-        precio: 11000,
-        img: "../assets/foto-productos/crizzy.jpg",
-        cantidad: 1
-    },
-    {
-        id: 11,
-        nombre: "Campera Brianna",
-        precio: 14000,
-        img: "../assets/foto-productos/brianna.jpg",
-        cantidad: 1
-    },
-    {
-        id: 12,
-        nombre: "Campera Rhoedia Neo",
-        precio: 9000,
-        img: "../assets/foto-productos/rhoedianeo.jpg",
-        cantidad: 1
-    },
-    {
-        id: 13,
-        nombre: "Campera Crom",
-        precio: 12000,
-        img: "../assets/foto-productos/crom.jpg",
-        cantidad: 1
-    },
-    {
-        id: 14,
-        nombre: "Campera Blair",
-        precio: 12000,
-        img: "../assets/foto-productos/blair.jpg",
-        cantidad: 1
-    },
-]
-
-let carrito =[]
-const contenedor = document.getElementById("contenedor")
 const carritoContenedor = document.getElementById("carritoContenedor")
 const precioCarrito = document.querySelector("precioTotal")
 const botonesAlert = document.getElementsByClassName("bton__producto")
 const btonComprar = document.getElementById("procesarCompra")
+
+
+// Visualizacion del carrito
 
 document.addEventListener("DOMContentLoaded", () => {
     carrito = JSON.parse(localStorage.getItem("carrito")) || []
@@ -132,11 +32,10 @@ const mostrarCarrito = () => {
         <hr>
         `
     })
-    
-    if(carrito.length === 0){
-        modalBody.innerHTML = `
-        <h5 class="text-center"> Aun no hay productos en el carrito</h5>
-        `
+        if(carrito.length === 0){
+            modalBody.innerHTML = `
+            <h5 class="text-center"> Aun no hay productos en el carrito</h5>
+            `
     }
 
     carritoContenedor.textContent = carrito.length
@@ -144,18 +43,9 @@ const mostrarCarrito = () => {
     guardarLocalStorage()
 }
 
-stock.forEach( (prod) => {
-    const {id, nombre, precio, img } = prod
-    contenedor.innerHTML += `
-    <div class="tarjeta col-12 col-sm-6 col-lg-4" >
-        <a href=""><img src="${img}" class="shadow p-3 mb-3 bg-body rounded producto__imagen"></a>
-        <p class="producto">${nombre}<br>${precio}</p>
-        <div class="bton">
-            <button onclick="agregarProducto(${id})" class="bton__producto" id="btonComprar">Agregar</button>
-        </div>
-    </div>
-    `
-})
+
+// Opciones de agregar y eliminar productos del carrito con sus alertas. Guardado del localstorage
+
 
 function agregarProducto(id){
 
@@ -170,7 +60,7 @@ function agregarProducto(id){
             
         })
     } else {
-        const producto =  stock.find((prod) => prod.id === id)
+        const producto =  productos.find((prod) => prod.id === id)
         carrito.push(producto)
         Swal.fire({
             position: 'top-end',
@@ -197,6 +87,9 @@ function guardarLocalStorage(){
     localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
+
+// Botones con SweetAlert2
+
 btonComprar.addEventListener("click", ( ) =>{
     if(carrito.length !== 0) {
         Swal.fire({
@@ -212,8 +105,44 @@ btonComprar.addEventListener("click", ( ) =>{
 }}
 )
 
-// Entrega final
+// Feths - Creacion y Publicacion de las Cards de los productos
 
-fetch('./local.json')
+const productos = []
+
+class Producto{
+    constructor(id, nombre, precio, img, cantidad){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.img = img
+        this.cantidad = cantidad
+    }
+    publicarProducto(){
+        const card = `
+        <div class="tarjeta col-12 col-sm-6 col-lg-4" >
+            <a href=""><img src="${this.img}" class="shadow p-3 mb-3 bg-body rounded producto__imagen"></a>
+            <p class="producto">${this.nombre}<br>$${this.precio}</p>
+            <div class="bton">
+                <button onclick="agregarProducto(${this.id})" class="bton__producto" id="btonComprar">Agregar</button>
+            </div>
+        </div>
+        `
+        const contenedor = document.getElementById("contenedor") 
+        contenedor.innerHTML += card
+        
+    }
+}
+
+fetch("../local.json")
     .then((response) => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.forEach( prod => {
+            let nuevoProducto = new Producto (prod.id, prod.nombre, prod.precio, prod.img, prod.cantidad)
+            productos.push(nuevoProducto)
+        })
+        productos.forEach( e =>{
+            e.publicarProducto()
+        })
+    })
+    
+    
